9a3e257bed6a0a404fd839e4037d8974a9a45ff2
Dmitry Bolonikov <dzmitry.balonikau@quadcode.com>
2024-02-09 21:38:00 +0300

    Initial Commit

---
+++ ImageFeed/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ImageFeed
+//
+//  Created by Dmitry Bolonikov on 9.02.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
